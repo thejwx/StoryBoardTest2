8d8cfcfdb9edef2a4c9b9b273320b685311703fc
Jeff Wilson <whackscript@gmail.com>
2011-12-04 14:53:57 -0600

    Initial Commit

---
+++ StoryBoardTest2/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  StoryBoardTest2
+//
+//  Created by Jeff Wilson on 12/4/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
